1b606501d6184815bf713f7032d72bbee6360860
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-08-22 11:06:56 +0600

    added object of vehecals

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-console.log("who are you bro what's your name?");
+var carObjec = {
+  vehecal: "car",
+  Name: "BMW",
+  fearPerkilo: 3,
+  imageUrl:
+    "https://images.unsplash.com/photo-1646029642262-022158ff5794?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+  description:
+    "Cars have controls for driving, parking, passenger comfort, and a variety of lights. Over the decades, additional features and controls have been added to vehicles, making them progressively more complex. These include rear-reversing cameras, air conditioning, navigation systems, and in-car entertainment. Most cars in use in the early 2020s are propelled by an internal combustion engine, fuelled by the combustion of fossil fuels.",
+  capcity: 4,
+};
+
+var bikeObjec = {
+  vehecal: "bike",
+  Name: "R15",
+  fearPerkilo: 2,
+  imageUrl:
+    "https://images.unsplash.com/photo-1558981001-5864b3250a69?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80",
+  description:
+    "Modern motorcycles even low-priced commuter motorcycles these days are featured with various updated features as well as a convenient range of the latest technologies. Hence Halogen bulbs in motorcycle headlamps are considered quite an updated addition featured very commonly. Halogen bulbs in motorcycle headlamps offer various advancements no doubt, but in counter, they also have a few demerits. Therefore covering those here we’re featuring demerits of halogen bulbs in motorcycle headlamp systems.",
+  capcity: 2,
+};
+var busObjec = {
+  vehecal: "Bus",
+  Name: "Anina",
+  fearPerkilo: 1,
+  imageUrl:
+    "https://images.unsplash.com/photo-1544620347-c4fd4a3d5957?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1169&q=80",
+  description:
+    "Classic buses for sale might need to have wheelchair accommodations added to support passengers with physical disabilities. The bus supply companies that offer wheelchair lifts and tie-downs have the necessary mobility parts and supplies to meet these needs. The electronic lifts make it easy to get the wheelchair on-board. Tie-downs secure the wheelchair safely in place during transport. This is key for public transportation.",
+  capcity: 36,
+};
+
+function vehecalCar() {
+  console.log(busObjec);
+}
+vehecalCar();
