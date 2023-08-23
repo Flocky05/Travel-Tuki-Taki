4814ceee5c6f7454517ce26a7db8d9f4c2baf515
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-08-23 14:42:57 +0600

    added body hero section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var carObjec = {
+var carObject = {
   vehecal: "car",
   Name: "BMW",
   fearPerkilo: 3,
@@ -9,7 +9,7 @@ var carObjec = {
   capcity: 4,
 };
 
-var bikeObjec = {
+var bikeObject = {
   vehecal: "bike",
   Name: "R15",
   fearPerkilo: 2,
@@ -19,7 +19,7 @@ var bikeObjec = {
     "Modern motorcycles even low-priced commuter motorcycles these days are featured with various updated features as well as a convenient range of the latest technologies. Hence Halogen bulbs in motorcycle headlamps are considered quite an updated addition featured very commonly. Halogen bulbs in motorcycle headlamps offer various advancements no doubt, but in counter, they also have a few demerits. Therefore covering those here we’re featuring demerits of halogen bulbs in motorcycle headlamp systems.",
   capcity: 2,
 };
-var busObjec = {
+var busObject = {
   vehecal: "Bus",
   Name: "Anina",
   fearPerkilo: 1,
@@ -30,7 +30,27 @@ var busObjec = {
   capcity: 36,
 };
 
-function vehecalCar() {
-  console.log(busObjec);
+/* function vehecalCar(service) {
+  const mainSection = document.getElementById("main-section");
+  const div = document.createElement("div");
+  div.innertHTML = `<div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://images.unsplash.com/photo-1583508805133-8fd03a9916d4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+      </div>
+    </div>
+  </div>
+</div>`;
+  mainSection.appendChild(div);
+  console.log(service);
 }
-vehecalCar();
+vehecalCar(carObject);
+ */
+let p = document.createElement("P");
+document.body.appendChild(p);
